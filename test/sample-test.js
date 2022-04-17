44dbe38b54c054af4dd122534b820df7fe10aba6
Sotatek-ThuyetNguyen <thuyet.nguyen@sotatek.com>
2022-04-18 00:37:13 +0700

    Update: write some UT

--- test/sample-test.js
+++ test/sample-test.js
@@ -2,18 +2,21 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("LTigerToken", function () {
-  it("Should return the new greeting once it's changed", async function () {
+  let ltiger;
+
+  beforeEach(async function () {
     const LTigerToken = await ethers.getContractFactory("LTigerToken");
-    const ltiger = await LTigerToken.deploy("Hello, world!");
+    ltiger = await LTigerToken.deploy();
     await ltiger.deployed();
-
-    expect(await ltiger.greet()).to.equal("Hello, world!");
-
-    const setGreetingTx = await ltiger.setGreeting("Hola, mundo!");
-
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
-
-    expect(await ltiger.greet()).to.equal("Hola, mundo!");
   });
+
+  describe("Get static contract info", function () {
+    it("Should return the name of contract", async function () {
+      expect(await ltiger.name()).to.equal("Little Tiger");
+    });
+
+    it("Should return the symbol of contract", async function () {
+      expect(await ltiger.symbol()).to.equal("LT");
+    });
+  })
 });
